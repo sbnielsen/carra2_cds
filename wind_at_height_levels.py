2b2e7d36272ad73f2a10a3667fc318709a99c123
Kristian Pagh Nielsen <78021394+kristianpagh@users.noreply.github.com>
2026-06-09 11:12:24 +0200

    Add files via upload
    Updated with working version

--- wind_at_height_levels.py
+++ wind_at_height_levels.py
@@ -3,26 +3,40 @@ import cdsapi
 dataset = "reanalysis-pan-carra"
 request = {
     "level_type": "height_levels",
-    "variable": ['wind_direction', 'wind_speed',],
-    "height_level": [
-            '100_m', '150_m', '15_m',
-            '200_m', '250_m', '300_m',
-            '30_m', '400_m', '500_m',
-            '50_m', '75_m',
-                    ],
-    "product_type': 'forecast",
+    "level_location": [
+        "15",
+        "30",
+        "50",
+        "75",
+        "100",
+        "150",
+        "200",
+        "250",
+        "300",
+        "400",
+        "500"
+    ],
+    "variable": [
+        "wind_direction",
+        "wind_speed"
+    ],
+    "product_type": "forecast",
     "time": [
-            '00:00', '03:00', '06:00',
-            '09:00', '12:00', '15:00',
-            '18:00', '21:00',
-            ],
-    "leadtime_hour": ['1', '2', '3',],
-    "year": '2008',
-    "month": '03',
-    "day": ['03', '04',],
-    "data_format": 'netcdf',
+        "00:00", "03:00", "06:00",
+        "09:00", "12:00", "15:00",
+        "18:00", "21:00"
+    ],
+    "leadtime_hour": [
+        "1",
+        "2",
+        "3"
+    ],
+    "year": ["2008"],
+    "month": ["03"],
+    "day": ["03", "04"],
+    "data_format": "netcdf",
     "area": [84, -142, 41, -52]
 }
- 
+
 client = cdsapi.Client()
 client.retrieve(dataset, request).download()
